6ff627f4fa9ebd6ef34348f94eb6a9531cf3c332
Edvin Norling <edvin.norling@xenit.se>
2022-12-01 11:06:36 +0100

    Start using 2022.12.1 tf release

--- aws/aws-core/main.tf
+++ aws/aws-core/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 module "core" {
-  source                       = "github.com/xenitab/terraform-modules//modules/aws/eks-core?ref=2022.11.2"
+  source                       = "github.com/xenitab/terraform-modules//modules/aws/eks-core?ref=2022.12.1"
   environment                  = var.environment
   name                         = var.core_name
   dns_zone                     = var.dns_zones
--- aws/azure-hub/ghrunner.tf
+++ aws/azure-hub/ghrunner.tf
@@ -1,5 +1,5 @@
 module "ghrunner" {
-  source            = "github.com/xenitab/terraform-modules//modules/azure/github-runner?ref=2022.11.2"
+  source            = "github.com/xenitab/terraform-modules//modules/azure/github-runner?ref=2022.12.1"
   environment       = var.environment
   location_short    = var.azure_location_short
   name              = "ghrunner"
