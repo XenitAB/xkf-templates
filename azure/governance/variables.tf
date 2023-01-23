8e88bb32d947f5d92ccba718087b35b44b9e281f
Carl Andersson <carl.andersson@xenit.se>
2023-01-23 10:56:01 +0100

    Add changes to template

--- azure/governance/variables.tf
+++ azure/governance/variables.tf
@@ -24,6 +24,11 @@ variable "unique_suffix" {
   type        = string
 }
 
+variable "azure_ad_group_prefix" {
+  description = "Prefix for Azure AD Groupss"
+  type        = string
+}
+
 variable "aks_group_name_prefix" {
   description = "Prefix for AKS Azure AD groups"
   type        = string
