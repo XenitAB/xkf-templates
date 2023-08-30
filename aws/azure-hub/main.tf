6073659312b74ac8f1c6c563ed727dcf0cc0a67f
Carl Andersson <carl.andersson@xenit.se>
2023-08-30 11:35:46 +0200

    Fix providers

--- aws/azure-hub/main.tf
+++ aws/azure-hub/main.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.57.0"
+      version = "3.71.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.28.1"
+      version = "2.41.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "4.0.3"
+      version = "4.0.4"
     }
   }
 }
